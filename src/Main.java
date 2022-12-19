615d50a600fff425e5caf9503460b9c769f41a7b
Александр <perchatki79@gmail.com>
2022-12-19 21:54:42 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+            task1();
+            task2();
+        }
+
+        public static void task1 () {
+            System.out.println("Задача 1");
+            var dog = 8.0;
+            var cat = 3.6;
+            var paper = 763789;
+            System.out.println(dog);
+            System.out.println(cat);
+            System.out.println(paper);
+            dog = dog + 4;
+            cat = cat + 4;
+            paper = paper + 4;
+            System.out.println(dog);
+            System.out.println(cat);
+            System.out.println(paper);
+            dog = dog -3.5;
+            cat = cat -1.6;
+            paper = paper - 7639;
+            System.out.println(dog);
+            System.out.println(cat);
+            System.out.println(paper);
+        }
+            public static void task2 () {
+                System.out.println("Задача 2");
+                // Пишем код для задачи 2
+            }
+
+        }
+
